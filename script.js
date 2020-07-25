4664c51156b7d7d1ea3d1a2b77886fc4a190c122
Lindsey Gobert <lmgobert1@gmail.com>
2020-07-24 22:05:02 -0500

    final changes

--- script.js
+++ script.js
@@ -2,19 +2,92 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  var values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*(){<>?|";
-  
-  for (let i = 0; i < 10; i++) {
-        password = password + values.Math.floor(Math.random() * Math.floor(values.length - 1));
-    
+function generatePassword(passwordLength){
+  var upperCaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerCaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
+  var num = "123456789";
+  var specialChar = "!@#$%^&*(){<>?|";
+  var randomPassword = "";
+  // var allInputs = upperCaseAlphabet + lowerCaseAlphabet + num + specialChar;
+
+  var inputLower = confirm("Do you want to include lower case letters?")
+  if (inputLower === true) {
+    randomPassword += lowerCaseAlphabet;
+  }
+  var inputUpper = confirm("Do you want to include upper case letters?")
+  if (inputUpper === true) {
+    randomPassword += upperCaseAlphabet;
+  }
+  var inputNum = confirm("Do you want to include numbers?")
+  if (inputNum === true) {
+    randomPassword += num;
+  }
+  var inputChar = confimrm("Do you want to include special characters?")
+  if (inputChar === true) {
+    randomPassword += specialChar;
   }
 
-  passwordText.values = password;
+  var charCount = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"))
+  for (let i = 0; i < charCount; i++) {
+    let inputs = Math.floor(Math.random()*randomPassword.length);
+    allInputs += randomPassword.charAt(inputs)
+  }
+
+  return randomPassword
 
 }
+    
+  // }
+    
+
+
+  
+    
+
+  //   for (let i = 0; i < array.length; i++) {
+  //     var allChar = Math.floor(Math.random()*allChar.length);
+  //     randomPassword +=
+      
+  //   }
+  // }
+
+
+
+
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password")
+
+//   passwordText.value = password;
+  // var password = "";
+  // var passwordText = document.querySelector("#password");
+  
+
+  // var builtString = ""
+
+  // //boolean
+
+
+
+
+  
+
+  // passwordLength: function() {
+  //   alert()
+  // }
+
+  
+  
+  // for (let i = 0; i < passwordLength; i++) {
+  //       password = password + values.Math.floor(Math.random() * Math.floor(values.length - 1));
+    
+  // }
+
+  // passwordText.values = password;
+
+
+
+
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
